a932d1d57257cbba7b84580349e9129ca06807a9
Elizaveta <elizaveta.blokhina47@gmail.ru>
2018-02-07 15:56:35 +0500

    task 2

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo service gunicorn restart
