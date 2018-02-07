e6b32e52806d9cde86b8185b574fa25aee43c528
Elizaveta <elizaveta.blokhina47@gmail.ru>
2018-02-07 16:04:29 +0500

    task 2

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,1 @@
-from multiprocessing import cpu_count
-
 bind = "0.0.0.0:8080"
-workers = cpu_count()*2 + 1
--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo service gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:app
