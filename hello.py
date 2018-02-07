a7da640010be6428e17f9c745bb82bc9d03f06cf
Elizaveta <elizaveta.blokhina47@gmail.ru>
2018-02-07 15:44:00 +0500

    task 2

--- hello.py
+++ hello.py
@@ -3,9 +3,10 @@ from urllib.parse import parse_qsl
 def wsgi_application(environ, start_response):
     data = ''
     if environ['REQUEST_METHOD'] == 'GET':
-        params = parse_qsl(environ['QUERY_STRING'])
-        for item in params:
-           data += '{}={}\n'.format(item[0], item[1])
+        if environ['QUERY_STRING'] != '':
+            params = parse_qsl(environ['QUERY_STRING'])
+            for item in params:
+               data += '{}={}\n'.format(item[0], item[1])
     headers = [
         ('Content-Type','text/plain'),
         ('Content-Length', str(len(data)))]
